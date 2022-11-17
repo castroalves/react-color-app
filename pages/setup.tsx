57eacd7369b139fc20f9d635792ed30b8a067ef4
Cadu de Castro Alves <cadudecastroalves@gmail.com>
2022-11-17 18:12:19 +0100

    Improve setup page

--- pages/setup.tsx
+++ pages/setup.tsx
@@ -1,14 +1,50 @@
 import { useApp, Wrapper } from "@graphcms/app-sdk-react";
-import { Box, Button, Flex, Heading } from "@hygraph/baukasten";
+import {
+    Alert,
+    AlertLink,
+    Box,
+    Button,
+    Flex,
+    Heading,
+    Inline,
+    Stack,
+    Text,
+} from "@hygraph/baukasten";
+import { ExternalLink } from "@hygraph/icons";
 
 function Setup() {
     const { installation, updateInstallation } = useApp();
     if (installation.status === "COMPLETED") {
         return (
-            <Flex gap="16" flexDirection={["column", "row"]}>
-                <Box p="s">
-                    <Heading>React Color</Heading>
-                    <div>App is installed</div>
+            <Stack maxWidth="720" padding="2px">
+                <Stack gap="4">
+                    <Inline gap="8" alignItems="center">
+                        <img
+                            src="/react-color-logo.svg"
+                            width={30}
+                            alt="React Color Logo"
+                        />
+                        <Text fontWeight={700} as="h2">
+                            React Color Picker
+                        </Text>
+                    </Inline>
+                    <Text color="neutral.400" fontSize="copy">
+                        React Color Picker field for Hygraph, powered by React
+                        Color package.
+                    </Text>
+                    <Alert variantColor="info" showStatusIcon>
+                        You can now add the React Color Picker field to your
+                        schema.{" "}
+                        <AlertLink
+                            as="a"
+                            target="_blank"
+                            href={
+                                "https://github.com/castroalves/react-color-app"
+                            }
+                        >
+                            Learn more <Box as={ExternalLink} size="icon.16" />
+                        </AlertLink>
+                    </Alert>
                     <Button
                         onClick={() => {
                             updateInstallation({
@@ -19,8 +55,8 @@ function Setup() {
                     >
                         Save
                     </Button>
-                </Box>
-            </Flex>
+                </Stack>
+            </Stack>
         );
     }
     return <Install />;
@@ -30,19 +66,34 @@ function Install() {
     const { updateInstallation } = useApp();
 
     return (
-        <Flex gap="16" flexDirection={["column", "row"]}>
-            <Box p="s">
-                <Heading>React Color</Heading>
-
+        <Stack maxWidth="720" padding="2px">
+            <Stack gap="4">
+                <Inline gap="8" alignItems="center">
+                    <img
+                        src="/react-color-logo.svg"
+                        width={30}
+                        alt="React Color Logo"
+                    />
+                    <Text fontWeight={700} as="h2">
+                        React Color Picker
+                    </Text>
+                </Inline>
+                <Text color="neutral.400" fontSize="copy">
+                    React Color Picker field for Hygraph, powered by React Color
+                    package.
+                </Text>
                 <Button
                     onClick={() => {
-                        updateInstallation({ status: "COMPLETED", config: {} });
+                        updateInstallation({
+                            status: "COMPLETED",
+                            config: {},
+                        });
                     }}
                 >
                     Save
                 </Button>
-            </Box>
-        </Flex>
+            </Stack>
+        </Stack>
     );
 }
 
